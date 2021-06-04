461a696b75a1c6c28ea39ea007a4b5e343017818
Msyaifani <84662125+Msyaifani@users.noreply.github.com>
2021-06-04 23:06:02 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -37,7 +37,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Farisfold/bot-jst_salman/main/public/cls_model/model.json';
+        const path = 'https://github.com/Msyaifani/Bot_JST_Syaifan/tree/main/public/cls_model';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
